423b23b4a056aa78a94817f912e996d492d7fdef
BilkoYevgen <bilko.yevgen@gmail.com>
2022-12-07 16:41:17 +0200

    Add Homework-3 solution

---
+++ HW-03/main.py
@@ -0,0 +1,29 @@
+#1
+print("I love Python " * 42)
+
+#2
+age_in_month = 335
+
+#3
+age_in_years = age_in_month // 12
+print(age_in_years)
+
+#4
+my_name = "Yevgen"
+my_age = "Му name is " + my_name + " I'm " + str(age_in_years) + " years old"
+print(my_age)
+
+#5
+variable = 1
+print(variable > 2)
+print(variable <= 2)
+print(variable != 2)
+print(variable == 2)
+print(variable >= 2)
+
+#6
+a = 2
+b = 5
+c = 6
+d = str(a) +  str(b) +  str(c)
+print(d)
