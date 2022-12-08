3433dea089cb96c309ac3c3fba514d2574cfcab3
BilkoYevgen <bilko.yevgen@gmail.com>
2022-12-08 16:09:38 +0200

    Add hoAdd Homework-4 solution

---
+++ HW-04/main.py
@@ -0,0 +1,10 @@
+input_type_checker = input("Enter word or integer: ")
+
+if input_type_checker.isdigit():
+    if int(input_type_checker) % 2 == 0:
+        print("This is an even number")
+    else:
+        print("This is an odd number")
+else:
+    length = len(input_type_checker)
+    print(f"This is {length} lettеr word")
