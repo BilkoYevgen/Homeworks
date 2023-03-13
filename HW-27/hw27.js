e70d4c0facaf74d15fc8e280dedb8e49569e46e8
Yevgen Bilko <bilko.yevgen@gmail.com>
2023-03-13 14:46:02 +0200

    Add some changes on js file.

--- HW-27/hw27.js
+++ HW-27/hw27.js
@@ -1,4 +1,4 @@
-// 1. Додати на раніше створену сторінку кнопки “Додати в друзі”, “Написати повідомлення”, “Запропонувати роботу”.
+//  Додати на раніше створену сторінку кнопки “Додати в друзі”, “Написати повідомлення”, “Запропонувати роботу”.
 let sendMessageBtn = document.createElement("button");
 sendMessageBtn.innerHTML = "Написати повідомлення";
 sendMessageBtn.style.background = "purple"
@@ -17,37 +17,23 @@ jobOfferBtn.addEventListener("click", function () {
 document.body.appendChild(jobOfferBtn);
 
 // 2. Додати окремий елемент, що має відображати кількість друзів. Ця кількість має ініціалізуватися як рандомне число при завантаженні сторінки, а при натисканні на "Додати в друзі" збільшуватися на 1.
+// Зробити неможливим повторне натискання кнопки “Додати в друзі”, змінювати її назву на “Очікується підтвердження” одразу після натискання і робити неактивною.
 
-var friendCount = Math.floor(Math.random() * 100) + 1;
+let friendCount = Math.floor(Math.random() * 100) + 1;
 
-var addFriendBtn = document.getElementById("addFriendBtn");
-var friendCountSpan = document.getElementById("friendCount");
+let addFriendBtn = document.getElementById("addFriendBtn");
+let friendCountSpan = document.getElementById("friendCount");
 
 friendCountSpan.innerHTML = friendCount + " друзів";
 
 addFriendBtn.addEventListener("click", function () {
 
+    addFriendBtn.disabled = true;
+
+    addFriendBtn.innerHTML = "Очікується підтвердження";
+    addFriendBtn.style.backgroundColor = "gray";
+    addFriendBtn.style.color = "white";
+
     friendCount++;
     friendCountSpan.innerHTML = friendCount + " друзів";
 });
-
-//  Зробити неможливим повторне натискання кнопки “Додати в друзі”, змінювати її назву на “Очікується підтвердження” одразу після натискання і робити неактивною.
-
-// var friendCount = Math.floor(Math.random() * 100) + 1;
-//
-// var addFriendBtn = document.getElementById("addFriendBtn");
-// var friendCountSpan = document.getElementById("friendCount");
-//
-// friendCountSpan.innerHTML = friendCount + " друзів";
-//
-// addFriendBtn.addEventListener("click", function () {
-//
-//     addFriendBtn.disabled = true;
-//
-//     addFriendBtn.innerHTML = "Очікується підтвердження";
-//     addFriendBtn.style.backgroundColor = "gray";
-//     addFriendBtn.style.color = "white";
-//
-//     friendCount++;
-//     friendCountSpan.innerHTML = friendCount + " друзів";
-// });
