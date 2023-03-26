0e6645402f4300b908602bbe258061415ddd5e9a
Yevgen Bilko <bilko.yevgen@gmail.com>
2023-03-26 16:21:25 +0300

    Some changes was added

--- HW-30/main.py
+++ HW-30/main.py
@@ -21,8 +21,13 @@ url2 = ("https://geocoding-api.open-meteo.com/v1/search?name=")
 city_country = input("Please enter a city or country here: ")
 res2 = requests.get(f"{url2}{city_country}")
 data = res2.json()
-latitude = round(data["results"][0]["latitude"], 2)
-longitude = round(data["results"][0]["longitude"], 2)
-url_api = requests.get(f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m&current_weather=true&past_days=1&forecast_days=1&timezone=auto")
-data_api = url_api.json()
-print(f"Temperature in {city_country} is {data_api['current_weather']['temperature']} degree celsius!")
+if "results" in data:
+       latitude = round(data["results"][0]["latitude"], 2)
+       longitude = round(data["results"][0]["longitude"], 2)
+       url_api = requests.get(f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m&current_weather=true&past_days=1&forecast_days=1&timezone=auto")
+       data_api = url_api.json()
+       print(f"Temperature in {city_country} is {data_api['current_weather']['temperature']} degree celsius!")
+else:
+       print("Sorry, i cant find results for this input")
+
+
