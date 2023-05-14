9032d953c3eafedfed3446e0e85ca2eab253fb64
Yevgen Bilko <bilko.yevgen@gmail.com>
2023-05-14 20:29:48 +0300

    Add solution for homework-46

--- HW-38/homework/settings.py
+++ HW-38/homework/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
