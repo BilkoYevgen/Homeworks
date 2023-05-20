f138f243b7da1eaa25a820124d9a159d35747d85
Yevgen Bilko <bilko.yevgen@gmail.com>
2023-05-20 21:46:36 +0300

    Add solution for homework-49

--- HW-38/user/tests.py
+++ HW-38/user/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import User
 
-# Create your tests here.
+class UserTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create(first_name='Yevgen', last_name='Bilko', age=28)
+
+    def test_create_user(self):
+        self.assertTrue(self.user is not None)
+
+    def test_allowed_user(self):
+        resp = self.client.get(f'/api/users/{self.user.id}/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_delete_user(self):
+        resp = self.client.delete(f'/api/users/{self.user.id}/')
+        self.assertEqual(resp.status_code, 204)
